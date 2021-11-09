bd71b3498a391927f3ff84897f91f48cf3feff7f
jagrunewald <je_grunewald@hotmail.com>
2021-11-08 21:23:39 -0300

    Tela de explorar - Requisitos 67 ao 69

--- src/Pages/Explorer/Explore.js
+++ src/Pages/Explorer/Explore.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import HeaderWithoutSearch from '../../components/HeaderNoSearchInput';
 
 const Explore = () => (
   <div>
     <HeaderWithoutSearch page="Explorar" />
+    <Link to="/explorar/comidas">
+      <button
+        type="button"
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
+    </Link>
+    <Link to="/explorar/bebidas">
+      <button
+        type="button"
+        data-testid="explore-drinks"
+      >
+        Explorar Bebidas
+      </button>
+    </Link>
     <Footer />
   </div>
 );
