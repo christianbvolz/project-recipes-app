33180c3d4779938fd3d92428618b584f8795cf36
jagrunewald <je_grunewald@hotmail.com>
2021-11-10 20:11:39 -0300

    Tela de explorar Bebidas e Comidas - Requisitos 70 a 74

--- src/Pages/Explorer/ExploreDrinks.js
+++ src/Pages/Explorer/ExploreDrinks.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import HeaderWithoutSearch from '../../components/HeaderNoSearchInput';
 
 const ExploreDrinks = () => (
   <div>
     <HeaderWithoutSearch page="Explorar Bebidas" />
+    <Link
+      data-testid="explore-by-ingredient"
+      to="/explorar/bebidas/ingredientes"
+    >
+      Por Ingredientes
+    </Link>
+    <Link
+      data-testid="explore-surprise"
+      to="/bebidas/178319"
+    >
+      Me Surpreenda!
+    </Link>
     <Footer />
   </div>
 );
--- src/Pages/Explorer/ExploreFood.js
+++ src/Pages/Explorer/ExploreFood.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
-
 import HeaderWithoutSearch from '../../components/HeaderNoSearchInput';
 
 const ExploreFood = () => (
   <div>
     <HeaderWithoutSearch page="Explorar Comidas" />
+    <Link
+      data-testid="explore-by-ingredient"
+      to="/explorar/comidas/ingredientes"
+    >
+      Por Ingredientes
+    </Link>
+    <Link
+      data-testid="explore-by-area"
+      to="/explorar/comidas/area"
+    >
+      Por Local de Origem
+    </Link>
+    <Link
+      data-testid="explore-surprise"
+      to="/comidas/52771"
+    >
+      Me Surpreenda!
+    </Link>
     <Footer />
   </div>
 );
